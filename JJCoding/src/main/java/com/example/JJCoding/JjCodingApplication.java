5fbef1a130aa4265427796a239fba76b0b9b999c
JIIN <94503845+JIINcho@users.noreply.github.com>
2024-09-04 12:46:44 +0900

    first commit

---
+++ JJCoding/src/main/java/com/example/JJCoding/JjCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.JJCoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjCodingApplication.class, args);
+	}
+
+}
---
+++ JJCoding/src/test/java/com/example/JJCoding/JjCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JJCoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
